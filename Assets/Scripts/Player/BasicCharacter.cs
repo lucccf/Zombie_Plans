9f861dc7b80757c676d07345768ad7d02c825cef
unknown <ruiliu@intranet.123u.com>
2023-04-05 23:12:30 +0800

    v0.3.11

---
+++ Assets/Scripts/Player/BasicCharacter.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using static Rigid_ctrl;
+
+public class BasicCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected PlayerStatus status = new PlayerStatus(100,10);
+    //碰撞体变量
+    public Fix_col2d f;
+    public Fix_rig2d r;
+    public long id;
+
+    protected Queue<Fix_col2d_act> AttackQueue = new Queue<Fix_col2d_act>();
+    protected Queue<Fix_col2d_act> TriggerQueue = new Queue<Fix_col2d_act>();
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void Updatex()
+    {
+
+    }
+
+    protected void CreateAttack(Fix_vector2 pos, Fixpoint wide, Fixpoint high, Fixpoint HpDamage, int toughness, float Toward)
+    {
+        Main_ctrl.NewAttack(pos, new Fix_vector2(0, 0), wide, high, HpDamage, toughness, id, Toward , false);
+    }
+
+    protected void CreateAttackWithCharacter(Fix_vector2 pos , Fix_vector2 with_pos, Fixpoint wide, Fixpoint high, Fixpoint HpDamage, int toughness, float Toward)
+    {
+        Main_ctrl.NewAttack(pos, with_pos, wide, high, HpDamage, toughness, id, Toward, true);
+    }
+
+    protected void GetColider()
+    {
+        if (((Fix_col2d)Main_ctrl.All_objs[id].modules[Object_ctrl.class_name.Fix_col2d]).actions.Count > 0)
+        {
+            Fix_col2d_act a = ((Fix_col2d)Main_ctrl.All_objs[id].modules[Object_ctrl.class_name.Fix_col2d]).actions.Dequeue();
+            if (a.type == Fix_col2d_act.col_action.Trigger_in || a.type == Fix_col2d_act.col_action.Trigger_out)
+            {
+                TriggerQueue.Enqueue(a);
+            }
+            else if (a.type == Fix_col2d_act.col_action.Attack)
+            {
+                AttackQueue.Enqueue(a);
+            }
+        }
+    }
+
+    private void Preform(int damage)
+    {
+        GameObject beat = (GameObject)AB.getobj("beat");
+        beat.transform.localScale = new Vector3(3f, 3f, 1f);
+        Instantiate(beat, transform.position, transform.rotation);
+        GameObject num = (GameObject)AB.getobj("HurtNumber");
+        GameObject num2 = Instantiate(num, transform.position + new Vector3(0f, 1f, 0f), Quaternion.identity);
+        num2.GetComponent<BeatNumber>().ChangeNumber(damage);
+    }
+
+    protected bool GetHited(float Toward)
+    {
+
+        GetColider();
+
+        bool this_hited = false;
+        while (AttackQueue.Count > 0)
+        {
+            Fix_col2d_act a = AttackQueue.Peek();
+            AttackQueue.Dequeue();
+            if (a.type != Fix_col2d_act.col_action.Attack)
+            {
+                Debug.Log("Attack Geted Error");
+                continue;
+            }
+            long AttackId = a.opsite.id;
+            if (!Main_ctrl.All_objs.ContainsKey(AttackId)) continue;
+            Attack attack = (Attack)(Main_ctrl.All_objs[AttackId].modules[Object_ctrl.class_name.Attack]);
+            if (attack.attakcer_id == id) continue;
+            Toward = attack.toward;
+            this_hited = true;
+
+            Preform(attack.HpDamage.to_int());
+
+            Fixpoint HpDamage = attack.HpDamage;
+            int ToughnessDamage = attack.ToughnessDamage;
+            status.GetAttacked(HpDamage, ToughnessDamage);
+
+            if (attack.type == 1)
+            {
+                Attack2 attack2 = (Attack2)attack;
+                attack2.DestroySelf();
+            }
+        }
+        return this_hited;
+    }
+    protected void RemoveHited()
+    {
+        GetColider();
+
+        if (AttackQueue.Count > 0)
+        {
+            AttackQueue.Dequeue();
+        }
+    }
+
+    protected void DeathFall(string name,int num,float size)
+    {
+        Main_ctrl.NewItem(f.pos.Clone(), name, num, size);
+    }
+}
