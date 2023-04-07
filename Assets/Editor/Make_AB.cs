d63fec5c3306ad3413ac32df11d12dd4baeb04b8
lucccf <2652526433@qq.com>
2023-04-07 14:57:19 +0800

    v0.3.26

--- Assets/Editor/Make_AB.cs
+++ Assets/Editor/Make_AB.cs
@@ -9,13 +9,16 @@ public class Make_AB : EditorWindow
     [MenuItem("Tools/Batch Build AssetBundles")]
     static void Init()
     {
-        string assetBundleDirectory = "AssetBundles";
+        string assetBundleDirectory = Application.dataPath + "/../AssetBundles";
         if (!Directory.Exists(assetBundleDirectory))
         {
             Directory.CreateDirectory(assetBundleDirectory);
         }
 
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        int bundleCount = manifest.GetAllAssetBundles().Length;
+        Debug.LogFormat("打包了 {0} 个AssetBundle", bundleCount);
+
         AssetDatabase.Refresh();
     }
 }
