f068cbf0d973ef0432c5cc95d7f6e2471640517e
lucccf <2652526433@qq.com>
2023-04-17 10:27:26 +0800

    0.7.1

---
+++ Assets/Scripts/UIButton/Option.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Option : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject op;
+    public Button but;
+    void Start()
+    {
+        but.onClick.AddListener(opt);
+    }
+
+    void opt()
+    {
+        op.SetActive(true);
+    }
+}
