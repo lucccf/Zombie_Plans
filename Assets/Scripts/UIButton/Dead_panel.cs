0fb34b9397d26f0a543fbb7d58e4418bc02cf79f
lucccf <2652526433@qq.com>
2023-04-18 10:50:46 +0800

    v0.7.19

---
+++ Assets/Scripts/UIButton/Dead_panel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dead_panel : MonoBehaviour
+{
+    public GameObject dr, def, de, vic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dr.SetActive(false);
+        def.SetActive(false);
+        de.SetActive(false);
+        vic.SetActive(false);
+    }
+
+    public void draw()
+    {
+        dr.SetActive(true);
+    }
+
+    public void defeated()
+    {
+        def.SetActive(true);
+    }
+
+    public void dead()
+    {
+        de.SetActive(true);
+    }
+
+    public void victory()
+    {
+        vic.SetActive(true);
+    }
+}
