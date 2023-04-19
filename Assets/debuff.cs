ce313c313786a2303680053db9d0b13663a23c61
Your Name <you@example.com>
2023-04-19 16:33:05 +0800

    v0.7.54

--- Assets/debuff.cs
+++ Assets/debuff.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class debuff : MonoBehaviour
 {
+    public ParticleSystem debuffparticle;
+    public float t;
     void Start()
     {
-        
+        debuffparticle = gameObject.GetComponent<ParticleSystem>();
+        t = Time.time;
+        debuffparticle.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
+/*        if (Input.GetKeyDown(KeyCode.Space)) {
+            debuffparticle.Play(); 
+        }*/
     }
 }
