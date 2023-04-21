99ffbe2711055d8953d58dca3fc54c7ee282b301
lucccf <2652526433@qq.com>
2023-04-21 15:15:33 +0800

    v0.8.14

---
+++ Assets/Scripts/AB/Load_battle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_battle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject canvas;
+
+    void Start()
+    {
+        Instantiate((GameObject)AB.getobj("Battle_scene"), canvas.transform);
+    }
+}
---
+++ Assets/Scripts/AB/Load_start.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject canvas;
+
+    void Start()
+    {
+        Instantiate((GameObject)AB.getobj("Start_scene"), canvas.transform);
+    }
+}
