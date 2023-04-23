131f417aec9c99acbace6a83012492c44be7b800
lucccf <2652526433@qq.com>
2023-04-23 11:49:34 +0800

    v0.9.4

---
+++ Assets/Scripts/UIButton/Text_to_play.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text_to_play : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(Application.dataPath + "/Configs/text.xml");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
