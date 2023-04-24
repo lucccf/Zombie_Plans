c34be0bf9acfa65fd274e1102e716ee5adbb5f0c
unknown <ruiliu@intranet.123u.com>
2023-04-24 20:35:17 +0800

    v0.9.55

---
+++ Assets/Scripts/Player/WolfBoxInMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfBoxInMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject BoxUI;
+    public GameObject ButtonUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TriggerIn()
+    {
+        ButtonUI.SetActive(true);
+    }
+
+    public void Triggerout()
+    {
+        BoxUI.SetActive(false);
+        ButtonUI.SetActive(false);
+    }
+}
