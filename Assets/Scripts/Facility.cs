f7b02740f3f72d5fa74536809e9c8d0af2974274
Your Name <you@example.com>
2023-04-24 11:24:15 +0800

    v0.9.30

--- Assets/Scripts/Facility.cs
+++ Assets/Scripts/Facility.cs
@@ -7,11 +7,12 @@ public class Facility : BasicCharacter
     public Dictionary<int, int> materials;
     public Dictionary<int, int> commited;
     public int cond;
+    public bool repaired;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        repaired= false;
     }
 
     // Update is called once per frame
