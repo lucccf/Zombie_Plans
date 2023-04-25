ff763f33ea02e2373dd9e26b00573f594d9ed5c8
Your Name <you@example.com>
2023-04-25 16:33:03 +0800

    v0.10.1

---
+++ Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static Color Player1 = new Color(0,0,0);
+    
+}
--- Assets/Scripts/UIButton/Attack_change.cs
+++ Assets/Scripts/UIButton/Attack_change.cs
@@ -6,20 +6,27 @@ using UnityEngine.UI;
 
 public class Attack_change : MonoBehaviour
 {
-    public long sf_id;
-    public long op_id;
-    public int pl_id;
-    public Text txt;
+    public long sf_id;//唯一
+    public long op_id;//唯一
+    public int pl_id;//第几个
+    public Text usertxt;
+    public Text typetxt;
     // Start is called before the first frame update
+    private void Start()
+    {
+        typetxt.text = "友方";
+
+    }
+
     void Update()
     {
         if (Player_ctrl.checkattack((int)sf_id, (int)op_id))
         {
-            txt.text = "User " + (pl_id + 1) + " eveil";
+            typetxt.text = "敌人";
         }
         else
         {
-            txt.text = "User " + (pl_id + 1) + " friend";
+            typetxt.text = "友方";
         }
     }
 
